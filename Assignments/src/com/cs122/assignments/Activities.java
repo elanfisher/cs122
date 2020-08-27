5593463d170e88f87e52cfe1018911315e3f7b80
Elan <Elan@DESKTOP-0PTMDHO>
2020-08-27 14:47:52 -0400

    Sending...

---
+++ Assignments/src/com/cs122/assignments/Activities.java
@@ -0,0 +1,12 @@
+package com.cs122.assignments;
+
+public class Activities {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		System.out.println("TestTest");
+	}
+
+}
