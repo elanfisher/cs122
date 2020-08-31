15fd2b9878d37466ac9051b4590fae374d52768d
Elan <Elan@DESKTOP-0PTMDHO>
2020-08-31 14:34:56 -0400

    I hope this works. Let me know if I pushed right.

--- Assignments/src/com/cs122/assignments/Activities.java
+++ Assignments/src/com/cs122/assignments/Activities.java
@@ -1,12 +1,34 @@
 package com.cs122.assignments;
 
-public class Activities {
-
-	public static void main(String[] args) 
+public class Activities 
+{
+	private String activityName;
+	private int suggestedHours;
+	private String locationType;
+	
+	public Activities(String inActivityName, int inSuggestedHours, String inLocationType)
 	{
-		// TODO Auto-generated method stub
-		
-		System.out.println("TestTest");
+		activityName = inActivityName;
+		suggestedHours = inSuggestedHours;
+		locationType = inLocationType;
 	}
-
+	
+	public void printActivity()
+	{
+		System.out.println("The "+ locationType + " activity " + "is suggested for "+ suggestedHours + " hours");
+	}
+	
+	public void preferredActivity(String activityPreferred)
+	{
+		System.out.println("I prefer "+activityPreferred+" over "+activityName);
+	}
+	
+	public boolean getOddHours()
+	{
+		if(suggestedHours % 2 == 1)//odd
+		{
+			return true;
+		}
+		else {return false;}
+	}
 }
