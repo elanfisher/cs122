894e14a516df4ed1924d67e691d4433ff1c9970a
dhruv857 <dgandhi@pace.edu>
2020-09-14 09:16:26 -0400

    assignment 1 feedback

--- Assignments/src/com/cs122/assignments/Activities.java
+++ Assignments/src/com/cs122/assignments/Activities.java
@@ -1,34 +1,33 @@
 package com.cs122.assignments;
 
-public class Activities 
-{
+public class Activities {
 	private String activityName;
 	private int suggestedHours;
 	private String locationType;
-	
-	public Activities(String inActivityName, int inSuggestedHours, String inLocationType)
-	{
+
+	public Activities(String inActivityName, int inSuggestedHours, String inLocationType) {
 		activityName = inActivityName;
 		suggestedHours = inSuggestedHours;
 		locationType = inLocationType;
 	}
-	
-	public void printActivity()
-	{
-		System.out.println("The "+ locationType + " activity " + "is suggested for "+ suggestedHours + " hours");
+
+	public void printActivity() {
+		System.out.println("The " + locationType + " activity " + "is suggested for " + suggestedHours + " hours");
 	}
-	
-	public void preferredActivity(String activityPreferred)
-	{
-		System.out.println("I prefer "+activityPreferred+" over "+activityName);
+
+	public void preferredActivity(String activityPreferred) {
+		System.out.println("I prefer " + activityPreferred + " over " + activityName);
 	}
-	
-	public boolean getOddHours()
-	{
-		if(suggestedHours % 2 == 1)//odd
+
+	public boolean getOddHours() {
+		// good job on writing comments, try to write more comments, it makes much
+		// better code and will
+		// also help you understand later
+		if (suggestedHours % 2 == 1)// odd
 		{
 			return true;
+		} else {
+			return false;
 		}
-		else {return false;}
 	}
 }
