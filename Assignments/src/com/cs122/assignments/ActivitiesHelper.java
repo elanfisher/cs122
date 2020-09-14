72a4c0649a8aca05368c93f463f6615231cae786
dhruv857 <dgandhi@pace.edu>
2020-09-14 09:16:58 -0400

    assignment 1 feedback

--- Assignments/src/com/cs122/assignments/ActivitiesHelper.java
+++ Assignments/src/com/cs122/assignments/ActivitiesHelper.java
@@ -2,13 +2,12 @@ package com.cs122.assignments;
 
 public class ActivitiesHelper {
 
-	public static void main(String[] args) 
-	{
-		Activities running = new Activities("Running",2,"Trail");
-		
+	public static void main(String[] args) {
+		Activities running = new Activities("Running", 2, "Trail");
+
 		running.printActivity();
 		running.preferredActivity("Sailing");
-		
+		// you are missing the part where it prints odd or false
 		System.out.println(running.getOddHours());
 	}
 
