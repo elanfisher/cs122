9e611b507adb0992d1dfd97e074992ab33315c00
Elan <Elan@DESKTOP-0PTMDHO>
2020-09-23 01:37:35 -0400

    Not sure what you mean by "[non]present" but I sorted them.

---
+++ ClassLabs/src/com/cs122/classlabs/Chapter10/Programming.java
@@ -0,0 +1,40 @@
+package com.cs122.classlabs.Chapter10;
+
+import java.util.Random;
+
+public class Programming 
+{
+	public static void main(String[] args) 
+	{
+		/*
+		 * create a class called Programming 
+		 * - have 6 students assigned id and name
+		 *  -- sort them and find a student with an id that is present and not present
+		 */	
+		Students[] studentList = new Students[6];
+		Random rand = new Random();
+		
+		studentList[0] = new Students(rand.ints(1111, 9999).findFirst().getAsInt(), "Jack");
+		studentList[1] = new Students(rand.ints(1111, 9999).findFirst().getAsInt(), "Sarah");
+		studentList[2] = new Students(rand.ints(1111, 9999).findFirst().getAsInt(), "Riley");
+		studentList[3] = new Students(rand.ints(1111, 9999).findFirst().getAsInt(), "Sydney");
+		studentList[4] = new Students(rand.ints(1111, 9999).findFirst().getAsInt(), "Hannah");
+		studentList[5] = new Students(rand.ints(1111, 9999).findFirst().getAsInt(), "Ashante");
+		
+		Sorting<Students> sorts = new Sorting<Students>();
+		
+		System.out.println("---------not-sorted----------");
+		for (Students student : studentList)
+		{	
+			System.out.println(student); 
+		}
+		
+		sorts.selectionSort(studentList);
+		System.out.println("-----------sorted----------");
+		for (Students student : studentList)
+		{	
+			System.out.println(student); 
+		}
+		
+	}
+}
--- ClassLabs/src/com/cs122/classlabs/Chapter10/Students.java
+++ ClassLabs/src/com/cs122/classlabs/Chapter10/Students.java
@@ -1,5 +1,54 @@
 package com.cs122.classlabs.Chapter10;
 
-public class Students {
+public class Students implements Comparable<Students>
+{
+	/*
+	 * create a class called Students with properties `uid(integer) and name(String)
+	create a class called Programming - have 6 students assigned id and name -- sort them and find a student with an id that is present and not present
+	 */
+	
+	private int uid;
+	private String name;
+	
+	public Students(int inUid, String inName)
+	{
+		uid = inUid;
+		name = inName;
+	}
 
+	public int compareTo(Students o) 
+	{
+		int result; 
+		
+		if (uid == o.getUid()) {
+		   result = name.compareTo(o.getName());
+		}
+		else 
+		{
+		   if(uid > o.getUid())
+		   {
+			   return 1;
+		   }
+		   else
+		   {
+			   return -1;
+		   }
+		}
+		
+		return result;
+	}
+
+	private String getName() 
+	{
+		return name;
+	}
+
+	private int getUid() {
+		return uid;
+	}
+	
+	public String toString()
+	{
+		return uid + "\t" + name;
+	}
 }
