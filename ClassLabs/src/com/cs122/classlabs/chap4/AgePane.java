1857d3f26b2bcff7e230cb8259c7cd27159a0e5c
Elan <Elan@DESKTOP-0PTMDHO>
2020-10-14 15:44:06 -0400

    Final HW, +red text.

--- ClassLabs/src/com/cs122/classlabs/chap4/AgePane.java
+++ ClassLabs/src/com/cs122/classlabs/chap4/AgePane.java
@@ -7,13 +7,15 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
 public class AgePane extends GridPane
 {
     private Label result;
-    private TextField YOB;
-
+    private TextField yOB;
+    private Label cent;
+    
     public AgePane()
     {
         Font font = new Font(18);
@@ -30,36 +32,52 @@ public class AgePane extends GridPane
         result.setFont(font);
         GridPane.setHalignment(result, HPos.CENTER);
         
-        YOB = new TextField();
-        YOB.setFont(font);
-        YOB.setPrefWidth(70);
-        YOB.setAlignment(Pos.CENTER);
-        YOB.setOnAction(this::processReturn);
+        yOB = new TextField();
+        yOB.setFont(font);
+        yOB.setPrefWidth(70);
+        yOB.setAlignment(Pos.CENTER);
+        yOB.setOnAction(this::processReturn);
         
         setAlignment(Pos.CENTER);
         setHgap(20);
         setVgap(20);
         setStyle("-fx-background-color: cyan");
         
-        add(inputLabel, 0, 0);
-        add(YOB, 1, 0);
+        cent = new Label("");
+        cent.setTextFill(Color.RED);
+        inputLabel.setFont(font);
+        GridPane.setHalignment(cent, HPos.CENTER);
+        
+        add(inputLabel, 0, 0);        
+        add(yOB, 1, 0);
         add(outputLabel, 0, 1);
         add(result, 1, 1);
+        add(cent,0,2);
     }
     
     public void processReturn(ActionEvent event)
     {
-    	System.out.println(event.getSource());
-        int fahrenheitTemp = Integer.parseInt(YOB.getText());
-        if(fahrenheitTemp < 1900)
-        {
-        	result.setText("Invalid Age");
+    	//System.out.println(event.getSource());
+    	//if age is > 100 "good job you lived more than a century, 
+    	//if you enter a text throw "age should be a number"
+        try{
+
+        	int finalAge = Integer.parseInt(yOB.getText());
+        	
+        	if(finalAge < 1900)
+            {
+            	cent.setText("Good job! You lived over a century.");
+            
+            }
+            else
+            {
+            	int ageFinal = (2020-finalAge);
+                result.setText(ageFinal + "");
+            }
         }
-        else
+        catch(Exception e)
         {
-        	int ageFinal = (2020-fahrenheitTemp);
-            result.setText(ageFinal + "");
+        	cent.setText("Age should be a number.");
         }
-        
     }
 }
