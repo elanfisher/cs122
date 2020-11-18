d1825f0b6f9e91a483934c6488c5a275a77e49d2
Elan <Elan@DESKTOP-0PTMDHO>
2020-11-18 14:12:27 -0500

    Stack Finished in class.

---
+++ ClassLabs/src/com/cs122/classlabs/chap13/StackDriver.java
@@ -0,0 +1,23 @@
+package com.cs122.classlabs.chap13;
+
+public class StackDriver {
+
+	public static void main(String[] args) 
+	{
+		Stacks s1 = new Stacks();
+		
+		s1.push(1);
+		s1.push(2);
+		s1.push(3);
+		s1.push(4);
+		s1.push(5);
+		
+		s1.print();
+		
+		s1.pop();
+		
+		s1.print();
+		
+		System.out.println(s1.peek());
+	}
+}
---
+++ ClassLabs/src/com/cs122/classlabs/chap13/Stacks.java
@@ -0,0 +1,80 @@
+package com.cs122.classlabs.chap13;
+
+public class Stacks 
+{
+	private class Node{
+		int data;
+		Node next;
+		
+		Node(int x){
+			data = x;
+			next = null;
+		}
+	}
+	
+	Node top;
+	
+	Stacks(){ //top of the stack
+		top = null;
+	}
+	
+	public int peek() {
+		if(empty()) {
+			return -1;
+		}
+		else {
+			return top.data;
+		}
+	}
+	
+	public int pop() {
+		if(empty()) { //nothing there
+			return -1;
+		}
+		else {			
+			Node temp = top.next;
+			top.next = null; //remove the first node of the list
+			top = temp; //set the second node to be the top
+			return top.data;
+		}
+	}
+	
+	public void push(int x) {		
+		/*if(empty()) {
+			System.out.println("nothing to add onto");
+		}
+		else {
+			//set incoming node to point at top node
+			Node newNode = new Node(x);
+			newNode.next = top;
+			top = newNode;
+			//set incoming node as top
+		}	
+		*/	
+		Node newNode = new Node(x);
+		newNode.next = top;
+		top = newNode;
+	}
+	
+	public boolean empty() {//true of top is empty
+		if(top == null) {
+			return true;
+		}
+		return false;
+	}
+	
+	public void print() 
+	{
+		String result = "";
+
+        Node current = top;
+
+        while (current != null)
+        {
+            result += current.data + " -> ";
+            current = current.next;
+        }       
+
+        System.out.println(result);
+	}
+}
